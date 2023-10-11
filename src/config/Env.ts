e56baa6ee8910e8cd0ce793359b12e77c6cd0f5f
Vennis-F <hoanganhgo28062001@gmail.com>
2023-10-11 08:24:20 +0700

    fix env

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -3,7 +3,10 @@
  */
 export const Env = {
   NODE_ENV: process.env.NODE_ENV,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+  API_BASE_URL:
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_API_BASE_LOCAL_URL
+      : process.env.REACT_APP_API_BASE_CLOUD_URL,
 
   isProd() {
     return this.NODE_ENV === 'production'
