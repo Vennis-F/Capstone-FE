7f1c153fa85fbaa5813899ded301b32d9158e393
Hoang Loc <nguyenhoangloc1403@gmail.com>
2023-12-09 10:49:03 +0700

    Fix Sigup

--- src/features/auth/components/GuestSignupContainer.tsx
+++ src/features/auth/components/GuestSignupContainer.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import LockOpenIcon from '@mui/icons-material/LockOpen'
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Grid, Link, Paper, Typography } from '@mui/material'
 import Container from '@mui/material/Container'
+import Image from 'material-ui-image'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -24,8 +26,8 @@ export const GuestSignupContainer = () => {
   }, [navigate])
 
   return (
-    <Container maxWidth="xs">
-      <Box sx={{ textAlign: 'center', marginBottom: '40px' }}>
+    <Container maxWidth="lg">
+      {/* <Box sx={{ textAlign: 'center', marginBottom: '40px' }}>
         <LockOpenIcon sx={{ color: MainColor.YELLOW_500 }} fontSize="large" />
         <Typography variant="h5" sx={{ fontWeight: '600' }}>
           Đăng ký
@@ -57,14 +59,68 @@ export const GuestSignupContainer = () => {
           alignItems: 'center',
         }}
       >
-        {/* <Link sx={{ color: 'black', fontSize: '14px', cursor: 'pointer' }}>Quên mật khẩu?</Link> */}
+       
         <Link
           sx={{ color: 'black', fontSize: '14px', cursor: 'pointer' }}
           onClick={() => navigate('/guest-login')}
         >
           Đã có tài khoản? Đăng nhập
         </Link>
-      </Box>
+      </Box> */}
+      <Paper elevation={3} sx={{ textAlign: "center" }}>
+        <Grid container spacing={0}>
+          <Grid item xs={6} sx={{}}>
+            <Typography sx={{ fontWeight: "bold", marginBottom: "20px", marginTop: "20px", fontSize: "25px", color: MainColor.RED_450 }}>Đăng ký </Typography>
+            <Paper sx={{ paddingX: '100px', border: 0 }}>
+              <GuestSignupForm
+                onSubmitClick={async data => {
+                  setIsLoading(true)
+                  try {
+                    const response = await customerSignUp({ ...data, role: 'Customer' })
+                    toastSuccess({ message: 'Đăng ký thành công' })
+                    navigate(`/confirm-otp?email=${response.email}`)
+                  } catch (error) {
+                    showErrorResponseSaga({
+                      error,
+                      defaultMessage: 'Không đăng ký được, vui lòng kiểm tra lại thông tin',
+                    })
+                  }
+                  setIsLoading(false)
+                }}
+                isLoading={isLoading}
+              />
+            </Paper>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '10px',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Link
+                sx={{ color: 'black', fontSize: '14px', cursor: 'pointer', marginBottom: '20px', textDecoration: "none" }}
+                onClick={() => navigate('/guest-login')}
+              >
+                Đã có tài khoản?
+                <span style={{ textDecoration: "underline", fontWeight: "bold" }}>
+                  Đăng nhập
+                </span>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Image
+              src={
+                'https://cara.edu.vn/wp-content/uploads/2021/03/o1.jpg'
+              }
+              style={{ height: '100%', width: '100%', padding: 0 }}
+              imageStyle={{ height: '100%', width: '100%', borderRadius: 3 }}
+            />
+          </Grid>
+        </Grid>
+      </Paper>
     </Container>
   )
 }
--- src/features/auth/components/InstructorSignupContainer.tsx
+++ src/features/auth/components/InstructorSignupContainer.tsx
@@ -1,8 +1,11 @@
-import { Box, Link, Typography } from '@mui/material'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, Grid, Link, Paper, Typography } from '@mui/material'
 import Container from '@mui/material/Container'
+import Image from 'material-ui-image'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { MainColor } from 'libs/const/color'
 import { showErrorResponseSaga } from 'libs/utils/handle-saga-error'
 import { toastSuccess } from 'libs/utils/handle-toast'
 import { getAccessToken } from 'libs/utils/handle-token'
@@ -22,8 +25,8 @@ const InstructorSignupContainer = () => {
   }, [navigate])
 
   return (
-    <Container maxWidth="xs">
-      <Box sx={{ textAlign: 'left', marginBottom: '20px' }}>
+    <Container maxWidth="lg">
+      {/* <Box sx={{ textAlign: 'left', marginBottom: '20px' }}>
         <Typography
           variant="h6"
           fontWeight="bold"
@@ -62,14 +65,67 @@ const InstructorSignupContainer = () => {
           alignItems: 'center',
         }}
       >
-        {/* <Link sx={{ color: 'black', fontSize: '14px', cursor: 'pointer' }}>Quên mật khẩu?</Link> */}
         <Link
           sx={{ color: 'black', fontSize: '14px', cursor: 'pointer' }}
           onClick={() => navigate('/guest-login')}
         >
           Đã có tài khoản? Đăng nhập
         </Link>
-      </Box>
+      </Box> */}
+      <Paper elevation={3} sx={{ textAlign: "center" }}>
+        <Grid container spacing={0}>
+          <Grid item xs={6} sx={{}}>
+            <Typography sx={{ fontWeight: "bold", marginBottom: "20px", marginTop: "20px", fontSize: "25px", color: MainColor.RED_450 }}>Đăng ký giảng viên</Typography>
+            <Paper sx={{ paddingX: '100px', border: 0 }}>
+              <GuestSignupForm
+                onSubmitClick={async data => {
+                  setIsLoading(true)
+                  try {
+                    const response = await customerSignUp({ ...data, role: 'Instructor' })
+                    toastSuccess({ message: 'Đăng ký thành công' })
+                    navigate(`/confirm-otp?email=${response.email}&isInstructor=${true}`)
+                  } catch (error) {
+                    showErrorResponseSaga({
+                      error,
+                      defaultMessage: 'Không đăng ký được, vui lòng kiểm tra lại thông tin',
+                    })
+                  }
+                  setIsLoading(false)
+                }}
+                isLoading={isLoading}
+              />
+            </Paper>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '10px',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Link
+                sx={{ color: 'black', fontSize: '14px', cursor: 'pointer', marginBottom: '20px', textDecoration: "none" }}
+                onClick={() => navigate('/guest-login')}
+              >
+                Đã có tài khoản?
+                <span style={{ textDecoration: "underline", fontWeight: "bold" }}>
+                  Đăng nhập
+                </span>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Image
+              src={
+                'https://www.vietnamworks.com//hrinsider//wp-content/uploads/2021/06/shutterstock_434383288-1.jpg'
+              }
+              style={{ height: '100%', width: '100%', padding: 0 }}
+              imageStyle={{ height: '100%', width: '100%', borderRadius: 3 }}
+            />
+          </Grid>
+        </Grid>
+      </Paper>
     </Container>
   )
 }
