964a096b75a04c2a5964ed6911a482029afa3312
Vennis-F <hoanganhgo28062001@gmail.com>
2023-12-17 21:16:13 +0700

    feature: drawing

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -4,8 +4,19 @@ const DrawingCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null)
   const [isDrawing, setIsDrawing] = useState(false)
-  const [color, setColor] = useState('#000000') // Màu sắc mặc định
-  const [brushSize, setBrushSize] = useState(5) // Độ dày nét vẽ mặc định
+  const [color, setColor] = useState('#000000') // Default color
+  const [brushSize, setBrushSize] = useState(5) // Default brush size
+
+  const getOffset = (clientX: number, clientY: number) => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      const rect = canvas.getBoundingClientRect()
+      const offsetX = clientX - rect.left
+      const offsetY = clientY - rect.top
+      return { offsetX, offsetY }
+    }
+    return { offsetX: 0, offsetY: 0 }
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -17,18 +28,29 @@ const DrawingCanvas: React.FC = () => {
     }
   }, [])
 
-  const startDrawing = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+  const startDrawing = ({
+    nativeEvent,
+  }: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
+    const clientX = 'touches' in nativeEvent ? nativeEvent.touches[0].clientX : nativeEvent.clientX
+    const clientY = 'touches' in nativeEvent ? nativeEvent.touches[0].clientY : nativeEvent.clientY
+
     if (ctx) {
-      const { offsetX, offsetY } = nativeEvent
+      const { offsetX, offsetY } = getOffset(clientX, clientY)
       ctx.beginPath()
       ctx.moveTo(offsetX, offsetY)
       setIsDrawing(true)
     }
   }
 
-  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+  const draw = ({
+    nativeEvent,
+  }: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>) => {
     if (!isDrawing || !ctx) return
-    const { offsetX, offsetY } = nativeEvent
+
+    const clientX = 'touches' in nativeEvent ? nativeEvent.touches[0].clientX : nativeEvent.clientX
+    const clientY = 'touches' in nativeEvent ? nativeEvent.touches[0].clientY : nativeEvent.clientY
+
+    const { offsetX, offsetY } = getOffset(clientX, clientY)
     ctx.lineWidth = brushSize
     ctx.strokeStyle = color
     ctx.lineTo(offsetX, offsetY)
@@ -52,16 +74,19 @@ const DrawingCanvas: React.FC = () => {
     <div>
       <canvas
         ref={canvasRef}
-        width={800} // Điều chỉnh kích thước canvas theo ý muốn
+        width={800} // Adjust canvas size as needed
         height={600}
         onMouseDown={startDrawing}
         onMouseMove={draw}
         onMouseUp={endDrawing}
         onMouseOut={endDrawing}
+        onTouchStart={startDrawing}
+        onTouchMove={draw}
+        onTouchEnd={endDrawing}
         style={{ border: '1px solid black' }}
       />
       <div>
-        <label htmlFor="colorPicker">Chọn màu:</label>
+        <label htmlFor="colorPicker">Select color:</label>
         <input
           type="color"
           id="colorPicker"
@@ -70,7 +95,7 @@ const DrawingCanvas: React.FC = () => {
         />
       </div>
       <div>
-        <label htmlFor="brushSize">Độ dày nét vẽ:</label>
+        <label htmlFor="brushSize">Brush size:</label>
         <input
           type="range"
           id="brushSize"
@@ -80,7 +105,7 @@ const DrawingCanvas: React.FC = () => {
           onChange={e => setBrushSize(Number(e.target.value))}
         />
       </div>
-      <button onClick={clearCanvas}>Xóa trang</button>
+      <button onClick={clearCanvas}>Clear canvas</button>
     </div>
   )
 }
